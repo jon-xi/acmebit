8d9ef467f1d545d1b2f6b13bc6467003ae1e9628
AcmeBattle <50351254+AcmeBattle@users.noreply.github.com>
2019-05-07 14:48:46 +0800

    Add files via upload

---
+++ custom.ts
@@ -0,0 +1,444 @@
+/* jichang
+ * 使用此文件来定义自定义函数和图形块。
+ * 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
+ */
+
+
+
+
+//% weight=100 color=#1E90FF icon="\uf13d"
+namespace jichang {
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const PWM_CH = 0x06
+    const PRESCALE = 0xFE
+
+    const STEP_CHA_L = 2047
+    const STEP_CHA_H = 4095
+
+    const STEP_CHB_L = 1
+    const STEP_CHB_H = 2047
+
+    const STEP_CHC_L = 1023
+    const STEP_CHC_H = 3071
+
+    const STEP_CHD_L = 3071
+    const STEP_CHD_H = 1023
+
+    export enum Servos {
+        S1 = 0x08,
+        S2 = 0x09,
+        S3 = 0x0A,
+        S4 = 0x0B,
+        S5 = 0x0C,
+        S6 = 0x0D,
+        S7 = 0x0E,
+        S8 = 0x0F,
+        S9 = 0x04,
+        S10 = 0x05,
+        S11 = 0x06,
+        S12 = 0x07
+    }
+
+    export enum Motors {
+        M1A = 0x1,
+        M1B = 0x2,
+        M2A = 0x3,
+        M2B = 0x4
+    }
+
+    export enum Steppers {
+        STEP_M1 = 0x1,
+        STEP_M2 = 0x2
+    }
+
+    export enum PingUnit {
+        //% block="cm"
+        Centimeters,
+        //% block="μs"
+        MicroSeconds
+    }
+
+    export enum BMP280_I2C_ADDRESS {
+        //% block="0x76"
+        ADDR_0x76 = 0x76,
+        //% block="0x77"
+        ADDR_0x77 = 0x77
+    }
+
+    //% blockId=ultrasonic_sensor block="ultrasonic_sensor unit|%unit"
+    //% weight=91
+    export function sensor(unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(DigitalPin.P1, PinPullMode.PullNone);
+        pins.digitalWritePin(DigitalPin.P1, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(DigitalPin.P1, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(DigitalPin.P1, 0);
+
+        // read pulse
+        let distance = Math.round(pins.pulseIn(DigitalPin.P2, PulseValue.High, maxCmDistance * 42));
+        console.log("Distance: " + Math.round(distance / 42) + "cm");
+
+        basic.pause(50)
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.round(distance / 42);
+            default: return Math.round(distance);
+        }
+    }
+    let initialized = false
+
+
+    let BMP280_I2C_ADDR = BMP280_I2C_ADDRESS.ADDR_0x76
+
+    /**************************************Test BMP280*******************************/
+    function getUInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.UInt16LE);
+    }
+
+    function getInt16LE(reg: number): number {
+        pins.i2cWriteNumber(BMP280_I2C_ADDR, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP280_I2C_ADDR, NumberFormat.Int16LE);
+    }
+    let dig_T1 = getUInt16LE(0x88)
+    let dig_T2 = getInt16LE(0x8A)
+    let dig_T3 = getInt16LE(0x8C)
+    let dig_P1 = getUInt16LE(0x8E)
+    let dig_P2 = getInt16LE(0x90)
+    let dig_P3 = getInt16LE(0x92)
+    let dig_P4 = getInt16LE(0x94)
+    let dig_P5 = getInt16LE(0x96)
+    let dig_P6 = getInt16LE(0x98)
+    let dig_P7 = getInt16LE(0x9A)
+    let dig_P8 = getInt16LE(0x9C)
+    let dig_P9 = getInt16LE(0x9E)
+    i2cwrite(BMP280_I2C_ADDR, 0xF4, 0x2F)
+    i2cwrite(BMP280_I2C_ADDR, 0xF5, 0x0C)
+    let Temp = 0
+    let Press = 0
+
+    function getBMP280(): void {
+        let adc_T = (i2cread(BMP280_I2C_ADDR, 0xFA) << 12) + (i2cread(BMP280_I2C_ADDR, 0xFB) << 4) + (i2cread(BMP280_I2C_ADDR, 0xFC) >> 4)
+        let var1 = (((adc_T >> 3) - (dig_T1 << 1)) * dig_T2) >> 11
+        let var2 = (((((adc_T >> 4) - dig_T1) * ((adc_T >> 4) - dig_T1)) >> 12) * dig_T3) >> 14
+        let t = var1 + var2
+        Temp = Math.idiv(((t * 5 + 128) >> 8), 100)
+        var1 = (t >> 1) - 64000
+        var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * dig_P6
+        var2 = var2 + ((var1 * dig_P5) << 1)
+        var2 = (var2 >> 2) + (dig_P4 << 16)
+        var1 = (((dig_P3 * ((var1 >> 2) * (var1 >> 2)) >> 13) >> 3) + (((dig_P2) * var1) >> 1)) >> 18
+        var1 = ((32768 + var1) * dig_P1) >> 15
+        if (var1 == 0)
+            return; // avoid exception caused by division by zero
+        let adc_P = (i2cread(BMP280_I2C_ADDR, 0xF7) << 12) + (i2cread(BMP280_I2C_ADDR, 0xF8) << 4) + (i2cread(BMP280_I2C_ADDR, 0xF9) >> 4)
+        let _p = ((1048576 - adc_P) - (var2 >> 12)) * 3125
+        _p = Math.idiv(_p, var1) * 2;
+        var1 = (dig_P9 * (((_p >> 3) * (_p >> 3)) >> 13)) >> 12
+        var2 = (((_p >> 2)) * dig_P8) >> 13
+        Press = _p + ((var1 + var2 + dig_P7) >> 4)
+    }
+
+    /**
+     * get pressure
+     */
+    //% blockId="BMP280_GET_PRESSURE" block="BMP280 get pressure"
+    //% weight=80 blockGap=8
+    export function pressure(): number {
+        getBMP280();
+        return Press;
+    }
+
+    /**
+     * get temperature
+     */
+    //% blockId="BMP280_GET_TEMPERATURE" block="BMP280 get temperature"
+    //% weight=80 blockGap=8
+    export function temperature(): number {
+        getBMP280();
+        return Temp;
+    }
+
+    /**
+     * power on
+     */
+    //% blockId="BMP280_POWER_ON" block="BMP280_Power On"
+    //% weight=61 blockGap=8
+    export function PowerOn() {
+        i2cwrite(BMP280_I2C_ADDR, 0xF4, 0x2F)
+    }
+
+    /**
+     * power off
+     */
+    //% blockId="BMP280_POWER_OFF" block="BMP280_Power Off"
+    //% weight=60 blockGap=8
+    export function PowerOff() {
+        i2cwrite(BMP280_I2C_ADDR, 0xF4, 0)
+    }
+
+
+    /**
+     * set I2C address
+     */
+    //% blockId="BMP280_SET_ADDRESS" block="set BMP280 address %addr"
+    //% weight=50 blockGap=8
+    export function Address(addr: BMP280_I2C_ADDRESS) {
+        BMP280_I2C_ADDR = addr
+    }
+
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        for (let ids = 0; ids < 16; ids++) {
+            setPwm(ids, 0, 0);
+        }
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = PWM_CH + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+
+    function setStepper(index: number, dir: boolean): void {
+        if (index == 1) {
+            if (dir) {
+                setPwm(0, STEP_CHA_L, STEP_CHA_H);
+                setPwm(2, STEP_CHB_L, STEP_CHB_H);
+                setPwm(1, STEP_CHC_L, STEP_CHC_H);
+                setPwm(3, STEP_CHD_L, STEP_CHD_H);
+            } else {
+                setPwm(3, STEP_CHA_L, STEP_CHA_H);
+                setPwm(1, STEP_CHB_L, STEP_CHB_H);
+                setPwm(2, STEP_CHC_L, STEP_CHC_H);
+                setPwm(0, STEP_CHD_L, STEP_CHD_H);
+            }
+        } else {
+            if (dir) {
+                setPwm(4, STEP_CHA_L, STEP_CHA_H);
+                setPwm(6, STEP_CHB_L, STEP_CHB_H);
+                setPwm(5, STEP_CHC_L, STEP_CHC_H);
+                setPwm(7, STEP_CHD_L, STEP_CHD_H);
+            } else {
+                setPwm(7, STEP_CHA_L, STEP_CHA_H);
+                setPwm(5, STEP_CHB_L, STEP_CHB_H);
+                setPwm(6, STEP_CHC_L, STEP_CHC_H);
+                setPwm(4, STEP_CHD_L, STEP_CHD_H);
+            }
+        }
+    }
+
+    function stopMotor(index: number) {
+        setPwm((index - 1) * 2, 0, 0);
+        setPwm((index - 1) * 2 + 1, 0, 0);
+    }
+
+    /**
+     * Servo Execute
+     * @param index Servo Channel; eg: S1
+     * @param degree [0-180] degree of servo; eg: 0, 90, 180
+    */
+    //% blockId=robotbit_servo block="Servo|%index|degree %degree"
+    //% weight=100
+    //% degree.min=0 degree.max=180
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Servo(index: Servos, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        setPwm(index, 0, value)
+    }
+
+    /**
+     * Geek Servo
+     * @param index Servo Channel; eg: S1
+     * @param degree [-45-225] degree of servo; eg: -45, 90, 225
+    */
+    //% blockId=robotbit_gservo block="Geek Servo|%index|degree %degree"
+    //% weight=99
+    //% blockGap=50
+    //% degree.min=-45 degree.max=225
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function GeekServo(index: Servos, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let v_us = ((degree - 90) * 20 / 3 + 1500) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        setPwm(index + 7, 0, value)
+    }
+
+    //% blockId=superbit_stepper_degree block="Stepper |%index|degree %degree"
+    //% weight=90
+    export function StepperDegree(index: Steppers, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        setStepper(index, degree > 0);
+        degree = Math.abs(degree);
+        basic.pause(10240 * degree / 360);
+        MotorStopAll()
+    }
+
+    //% blockId=superbit_stepper_dual block="Dual Stepper(Degree) |STEP_M1 %degree1| STEP_M2 %degree2"
+    //% weight=89
+    export function StepperDual(degree1: number, degree2: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        setStepper(1, degree1 > 0);
+        setStepper(2, degree2 > 0);
+        degree1 = Math.abs(degree1);
+        degree2 = Math.abs(degree2);
+        basic.pause(10240 * Math.min(degree1, degree2) / 360);
+        if (degree1 > degree2) {
+            stopMotor(3); stopMotor(4);
+            basic.pause(10240 * (degree1 - degree2) / 360);
+        } else {
+            stopMotor(1); stopMotor(2);
+            basic.pause(10240 * (degree2 - degree1) / 360);
+        }
+
+        MotorStopAll()
+    }
+
+
+    //% blockId=superbit_motor_run block="Motor|%index|speed %speed"
+    //% weight=85
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRun(index: Motors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        speed = speed * 16; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+        if (index > 4 || index <= 0)
+            return
+        let pp = (index - 1) * 2
+        let pn = (index - 1) * 2 + 1
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
+
+
+    /**
+     * Execute two motors at the same time
+     * @param motor1 First Motor; eg: M1A, M1B
+     * @param speed1 [-255-255] speed of motor; eg: 150, -150
+     * @param motor2 Second Motor; eg: M2A, M2B
+     * @param speed2 [-255-255] speed of motor; eg: 150, -150
+    */
+    //% blockId=superbit_motor_dual block="Motor|%motor1|speed %speed1|%motor2|speed %speed2"
+    //% weight=84
+    //% speed1.min=-255 speed1.max=255
+    //% speed2.min=-255 speed2.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRunDual(motor1: Motors, speed1: number, motor2: Motors, speed2: number): void {
+        MotorRun(motor1, speed1);
+        MotorRun(motor2, speed2);
+    }
+
+    /**
+     * Execute single motors with delay
+     * @param index Motor Index; eg: M1A, M1B, M2A, M2B
+     * @param speed [-255-255] speed of motor; eg: 150, -150
+     * @param delay seconde delay to stop; eg: 1
+    */
+    //% blockId=superbit_motor_rundelay block="Motor|%index|speed %speed|delay %delay|s"
+    //% weight=81
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRunDelay(index: Motors, speed: number, delay: number): void {
+        MotorRun(index, speed);
+        basic.pause(delay * 1000);
+        MotorRun(index, 0);
+    }
+
+
+
+    //% blockId=superbit_stop block="Motor Stop|%index|"
+    //% weight=80
+    export function MotorStop(index: Motors): void {
+        MotorRun(index, 0);
+    }
+
+    //% blockId=superbit_stop_all block="Motor Stop All"
+    //% weight=79
+    //% blockGap=50
+    export function MotorStopAll(): void {
+        for (let ids = 1; ids <= 4; ids++) {
+            stopMotor(ids);
+        }
+    }
+
+
+}
+
+
+
+
+
